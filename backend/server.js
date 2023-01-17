1e2a5725330f296ac101bdbcd6664d04cf3f00e9
Damian <damianzimny93@gmail.com>
2023-01-17 13:18:54 +0100

    cors

--- backend/server.js
+++ backend/server.js
@@ -1,12 +1,12 @@
 const express = require("express");
-
+const cors = require("cors");
 const app = express();
-
+app.use(cors());
 app.get("/", (req, res) => {
   res.send("welcome from home");
 });
 app.get("/books", (req, res) => {
-  res.send("test");
+  res.send("hahahahahahahhahahaaiidhiagduogauodhguagdigaiduygiuagduagdiu");
 });
 app.listen(8000, () => {
   console.log("server is lestining...");
